3df7a06f0a93aa9f534f4c016deb388b897be90e
Alexander Sotomayor <alex15spineda@hotmail.com>
2025-02-14 10:32:33 -0600

    creating python file

---
+++ DQL_bj_agent.py
@@ -0,0 +1,8 @@
+import gymnasium as gym
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from collections import deque
